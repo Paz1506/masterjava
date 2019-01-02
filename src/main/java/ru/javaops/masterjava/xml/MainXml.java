65be8b4e7d9d98cded9f38445bb038f5c8fe974a
Paz1506 <Paz1506@gmail.com>
2019-01-02 15:10:45 +1000

    3 Доделал, почистить

--- src/main/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -11,10 +11,13 @@ import ru.javaops.masterjava.xml.schema.*;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
 
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MainXml {
 
@@ -25,26 +28,56 @@ public class MainXml {
     }
 
     public static void main(String[] args) throws IOException, JAXBException {
-        getUsersOfProjects("masterjava");
+        getUsersOfProjects("topjava");
     }
 
-    private static void getUsersOfProjects(String groupName) throws IOException, JAXBException {
+    private static void getUsersOfProjects(String projectName) throws IOException, JAXBException {
         Payload payload = JAXB_PARSER.unmarshal(
                 Resources.getResource("payload.xml").openStream());
 
         // юзеры
-        payload.getUsers().getUser().forEach(user -> System.out.println(user.getGroups().getGroup().get(0).getGroup()));
+        payload.getUsers().getUser().forEach(user -> System.out.println("User: "+ user + " |Groups: " + user.getGroups().getGroup().get(0).getGroup()));
         payload.getUsers().getUser().forEach(user -> System.out.println(user.getFullName()));
 
         //Ура! Ведут на один и тот же объект!
         // проекты
+        System.out.println("=== Projects ===");
         System.out.println(payload.getProjects().getProject().get(0));
+        System.out.println(payload.getProjects().getProject().get(1));
 
         // группы
-        payload.getGroups().getGroup().forEach(group -> System.out.println(group));
+        System.out.println("=== All groups ===");
+        payload.getGroups().getGroup().forEach(group -> System.out.println("Group: " + group + " | Project: " + group.getProject()));
 //        System.out.println(payload.getGroups().getGroup().get(0).getProject());
 
-//        List<User> userList = payload.getUsers().getUser();
-//        userList.forEach(user -> System.out.println(user.getCity().getGroup().get(0).getGroup()));
+        // Проект
+        Project project = payload.getProjects()
+                                 .getProject()
+                                 .stream()
+                                 .filter(project1 -> project1.getName()
+                                                             .equalsIgnoreCase(projectName))
+                                 .findFirst().get();
+        System.out.println("Project: " + project);
+
+        // Группы, принадл. этому проекту
+        List<Group> groups = payload.getGroups().getGroup().stream().filter(group -> group.getProject().equals(project)).collect(Collectors.toList());
+
+        List<User> users = payload.getUsers().getUser();
+
+        Set<User> result = new HashSet<>();
+
+        for (User user : users){
+            List<User.Groups.Group> groupList = user.getGroups().getGroup();
+            for (User.Groups.Group group : groupList){
+                System.out.println("Group: " + group + " | User: " + user);
+                if (groups.contains(group.getGroup())) {
+                    result.add(user);
+                }
+            }
+        }
+
+        System.out.println("result = " + result.size());
+
+//        List<User> userList = payload.getUsers().getUser().stream().filter(user -> user.getGroups().getGroup()).collect(Collectors.toList());
     }
 }
