a0bd06fa8738c2fd23bc2bd1e224104ae3da4261
Paz1506 <Paz1506@gmail.com>
2019-01-02 14:37:44 +1000

    3 первые ростки зла

--- src/main/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -7,13 +7,14 @@
 package ru.javaops.masterjava.xml;
 
 import com.google.common.io.Resources;
-import ru.javaops.masterjava.xml.schema.ObjectFactory;
-import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.schema.*;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
 
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.List;
 
 public class MainXml {
 
@@ -24,13 +25,26 @@ public class MainXml {
     }
 
     public static void main(String[] args) throws IOException, JAXBException {
-        getUsersOfGroup("masterjava01");
+        getUsersOfProjects("masterjava");
     }
 
-    private static void getUsersOfGroup(String groupName) throws IOException, JAXBException {
+    private static void getUsersOfProjects(String groupName) throws IOException, JAXBException {
         Payload payload = JAXB_PARSER.unmarshal(
                 Resources.getResource("payload.xml").openStream());
 
+        // юзеры
+        payload.getUsers().getUser().forEach(user -> System.out.println(user.getGroups().getGroup().get(0).getGroup()));
         payload.getUsers().getUser().forEach(user -> System.out.println(user.getFullName()));
+
+        //Ура! Ведут на один и тот же объект!
+        // проекты
+        System.out.println(payload.getProjects().getProject().get(0));
+
+        // группы
+        payload.getGroups().getGroup().forEach(group -> System.out.println(group));
+//        System.out.println(payload.getGroups().getGroup().get(0).getProject());
+
+//        List<User> userList = payload.getUsers().getUser();
+//        userList.forEach(user -> System.out.println(user.getCity().getGroup().get(0).getGroup()));
     }
 }
