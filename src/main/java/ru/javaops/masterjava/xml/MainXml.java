ebf27e71833594b3dd64c820cf981c68c554f99f
Paz1506 <Paz1506@gmail.com>
2019-01-02 15:19:05 +1000

    3 Почистил

--- src/main/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -13,7 +13,6 @@ import ru.javaops.masterjava.xml.util.Schemas;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,21 +34,6 @@ public class MainXml {
         Payload payload = JAXB_PARSER.unmarshal(
                 Resources.getResource("payload.xml").openStream());
 
-        // юзеры
-        payload.getUsers().getUser().forEach(user -> System.out.println("User: "+ user + " |Groups: " + user.getGroups().getGroup().get(0).getGroup()));
-        payload.getUsers().getUser().forEach(user -> System.out.println(user.getFullName()));
-
-        //Ура! Ведут на один и тот же объект!
-        // проекты
-        System.out.println("=== Projects ===");
-        System.out.println(payload.getProjects().getProject().get(0));
-        System.out.println(payload.getProjects().getProject().get(1));
-
-        // группы
-        System.out.println("=== All groups ===");
-        payload.getGroups().getGroup().forEach(group -> System.out.println("Group: " + group + " | Project: " + group.getProject()));
-//        System.out.println(payload.getGroups().getGroup().get(0).getProject());
-
         // Проект
         Project project = payload.getProjects()
                                  .getProject()
@@ -57,18 +41,23 @@ public class MainXml {
                                  .filter(project1 -> project1.getName()
                                                              .equalsIgnoreCase(projectName))
                                  .findFirst().get();
-        System.out.println("Project: " + project);
 
         // Группы, принадл. этому проекту
-        List<Group> groups = payload.getGroups().getGroup().stream().filter(group -> group.getProject().equals(project)).collect(Collectors.toList());
+        List<Group> groups = payload.getGroups()
+                                    .getGroup()
+                                    .stream()
+                                    .filter(group -> group.getProject()
+                                                          .equals(project))
+                                    .collect(Collectors.toList());
 
-        List<User> users = payload.getUsers().getUser();
+        List<User> users = payload.getUsers()
+                                  .getUser();
 
         Set<User> result = new HashSet<>();
 
-        for (User user : users){
+        for (User user : users) {
             List<User.Groups.Group> groupList = user.getGroups().getGroup();
-            for (User.Groups.Group group : groupList){
+            for (User.Groups.Group group : groupList) {
                 System.out.println("Group: " + group + " | User: " + user);
                 if (groups.contains(group.getGroup())) {
                     result.add(user);
---
+++ src/main/java/ru/javaops/masterjava/xml/schema/Group.java
@@ -0,0 +1,124 @@
+
+package ru.javaops.masterjava.xml.schema;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlIDREF;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}ID" />
+ *       &lt;attribute name="groupType" use="required" type="{http://javaops.ru}groupType" />
+ *       &lt;attribute name="project" use="required" type="{http://www.w3.org/2001/XMLSchema}IDREF" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "Group", namespace = "http://javaops.ru")
+public class Group {
+
+    @XmlAttribute(name = "name")
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+    @XmlID
+    @XmlSchemaType(name = "ID")
+    protected String name;
+    @XmlAttribute(name = "groupType", required = true)
+    protected GroupType groupType;
+    @XmlAttribute(name = "project", required = true)
+    @XmlIDREF
+    @XmlSchemaType(name = "IDREF")
+    protected Object project;
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the groupType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link GroupType }
+     *     
+     */
+    public GroupType getGroupType() {
+        return groupType;
+    }
+
+    /**
+     * Sets the value of the groupType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link GroupType }
+     *     
+     */
+    public void setGroupType(GroupType value) {
+        this.groupType = value;
+    }
+
+    /**
+     * Gets the value of the project property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Object }
+     *     
+     */
+    public Object getProject() {
+        return project;
+    }
+
+    /**
+     * Sets the value of the project property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Object }
+     *     
+     */
+    public void setProject(Object value) {
+        this.project = value;
+    }
+
+}
