f3dc8d2a1743bb88a84a1e1738488064cbbe0fa0
Paz1506 <Paz1506@gmail.com>
2019-01-08 13:09:49 +1000

    hw06. Отсылка простой почты.

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,64 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
 
 import java.util.List;
 
 @Slf4j
 public class MailSender {
+
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
         log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+
+        Email email = new SimpleEmail();
+        Config emailConfig = Configs.getConfig("mail.conf", "mail");
+        String host = emailConfig.getString("host");
+        Integer port = emailConfig.getInt("port");
+        String username = emailConfig.getString("username");
+        String password = emailConfig.getString("password");
+        Boolean useSSL = emailConfig.getBoolean("useSSL");
+        Boolean useTLS = emailConfig.getBoolean("useTLS");
+        Boolean debug = emailConfig.getBoolean("debug");
+        String fromName = emailConfig.getString("fromName");
+
+        try {
+            email.setHostName(host);
+            email.setSmtpPort(port);
+            email.setAuthenticator(new DefaultAuthenticator(username, password));
+            email.setSSLOnConnect(useSSL);
+            email.setTLS(useTLS);
+            email.setDebug(debug);
+            email.setFrom(username);
+            email.setSubject(subject);
+            email.setMsg(body);
+
+            to.forEach(t -> {
+                try {
+                    email.addTo(t.getEmail());
+                } catch (EmailException e) {
+                    log.info("Error add TO: " + t);
+                }
+            });
+
+            cc.forEach(c -> {
+                try {
+                    email.addCc(c.getEmail());
+                } catch (EmailException e) {
+                    log.info("Error add CC: " + c);
+                }
+            });
+
+            email.send();
+
+        } catch (EmailException e) {
+            log.info("Send mail is NOT SUCCESS :(");
+            e.printStackTrace();
+        }
     }
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -15,6 +15,7 @@ public class MailServiceClient {
                 new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
-        mailService.sendMail(ImmutableList.of(new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+//        mailService.sendMail(ImmutableList.of(new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+        mailService.sendMail(ImmutableList.of(new Addressee("paz1506@mail.ru", null)), null, "Subject", "Body from java client");
     }
 }
